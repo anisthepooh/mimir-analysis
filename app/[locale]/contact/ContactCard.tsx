4837e35d62b9083d96c97856065f9ff35c047946
Casper Anisimow <81159246+anisthepooh@users.noreply.github.com>
2024-12-10 14:44:18 +0100

    made contact ready for prod

--- app/[locale]/contact/ContactCard.tsx
+++ app/[locale]/contact/ContactCard.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 const ContactCard = ({author}: {author: any}) => {
   return (
-    <Card className="flex gap-4 p-8 w-full">
+    <Card className="flex gap-4 p-8 max-w-sm">
     <Avatar>
       {author.image ? (
         <AvatarImage src={author.image} alt={`${author.firstname} ${author.lastname}`} />
@@ -21,20 +21,20 @@ const ContactCard = ({author}: {author: any}) => {
         {author.firstname} {author.lastname}
       </h4>
       <p className="text-sm font-medium text-muted-foreground">{author.title}</p>
-      <div className="flex items-center pt-2 gap-2">
+      {author.phone && <div className="flex items-center pt-2 gap-2">
         <Phone size={12} className='text-muted-foreground' /> 
         <p className="text-sm">{author.phone}</p>
-      </div>
-      <div className="flex items-center pt-2 gap-2">
+      </div>}
+      {author.mail && <div className="flex items-center pt-2 gap-2">
         <MailIcon size={12} className='text-muted-foreground' /> 
         <p className="text-sm">{author.mail}</p>
-      </div>
-      <div className="flex items-center pt-2 gap-2">
+      </div>}
+      {author.link && <div className="flex items-center pt-2 gap-2">
         <Link size={12} className='text-muted-foreground' /> 
         <a href={author.website} className="text-sm text-blue-500 hover:underline">
           {author.website}
         </a>
-      </div>
+      </div>}
     </div>
   </Card>
   )
--- app/[locale]/contact/Content.tsx
+++ app/[locale]/contact/Content.tsx
@@ -16,7 +16,7 @@ const Content: React.FC<ContentProps> = ({ authors }) => {
       <div className='flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8'>
         <div className="flex-1 space-y-4">
           <h1 className="inline-block font-heading text-4xl tracking-tight lg:text-5xl">{t('contributors')}</h1>
-          <p className="text-xl text-muted-foreground">A blog built using Contentlayer. Posts are written in MDX.</p>
+          <p className="text-xl text-muted-foreground">{t('contact_page.description')}</p>
         </div>
       </div>
       <hr className="my-8"></hr>
